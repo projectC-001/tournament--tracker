7875f95d3fb27158cdd8a0101b3c0a262e62fa27
unknown <summitkhatiwada22>
2022-07-10 21:32:12 -0500

    Added forms for all level

---
+++ tournament-tracker/TournamentTracker/TrackerUI/CreatePrizeForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrackerUI
+{
+    public partial class CreatePrizeForm : Form
+    {
+        public CreatePrizeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CreatePrizeForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void firstNameValue_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void firstNameLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
